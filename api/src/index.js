807e6ae45c0ed62973b1381f8cf9d520e814f104
James Brooks <virverum22@gmail.com>
2024-01-24 00:43:33 -0500

    GraphQL basics

--- api/src/index.js
+++ api/src/index.js
@@ -4,24 +4,68 @@ const express = require('express');
 const app = express();
 //Create a port object that allows us to change the port number dynamically OR the port defaults to 4000
 const port = process.env.PORT || 4000;
+
+let notes = [
+    {
+      id: '1',
+      content: 'This is a note',
+      author: 'Adam Scott'
+    },
+    {
+      id: '2',
+      content: 'This is another note',
+      author: 'Harlow Everly'
+    },
+    {
+      id: '3',
+      content: 'Oh hey look, another note!',
+      author: 'Riley Harrison'
+    }
+  ];
+
 const { ApolloServer, gql} = require('apollo-server-express');
 
-//Construct a schema, using GraphQL schema language
+//Construct a schema, using GraphQL schema language (gql)
 const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
     type Query {
-        hello: String
+        hello: String!
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+    type Mutation {
+        newNote(content: String!): Note!
     }
 `;
 
 //Provide resolver functions for our schema fields
 const resolvers = {
     Query: {
-        hello: () => 'Hello World!'
+        hello: () => 'Hello World!',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+            }
+        },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content:args.content,
+                author: 'Adam Scott'
+                };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
 
 //Apollo Server setup
-const server = new ApolloServer({ typeDefs, resolver });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 //Apply the Apolla GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: '/api'})
